06895d04cb071f8810a44ab637d7d08b35045ae8
Sebastien Joseph <sebastienjoseph07@outlook.com>
2024-06-27 10:36:22 -0400

    create microservice for backend with user and assignment service

---
+++ assignmentservice/src/main/java/com/hcc/assignmentservice/AssignmentserviceApplication.java
@@ -0,0 +1,13 @@
+package com.hcc.assignmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentserviceApplication.class, args);
+	}
+
+}
---
+++ assignmentservice/src/test/java/com/hcc/assignmentservice/AssignmentserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcc.assignmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
