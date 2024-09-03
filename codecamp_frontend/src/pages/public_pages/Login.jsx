3dc80dfcd04bea9a7ea028360d74a281c4704858
Sebastien Joseph <sebastienjoseph07@outlook.com>
2024-09-03 13:40:39 -0400

    frontend: updated login page

--- codecamp_frontend/src/pages/public_pages/Login.jsx
+++ codecamp_frontend/src/pages/public_pages/Login.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import RedirectUrl from "../../components/RedirectUrl";
 import "../../css/Login.css";
+import Logo from "../../components/Logo";
 
 function Login() {
   const [username, setUsername] = useState("");
@@ -20,11 +21,11 @@ function Login() {
         { username, password }
       );
 
-      console.log("response", response)
       localStorage.setItem(
         "lmsusertoken",
         response.headers.authorization.split(" ")[1]
       );
+
       const userAuthority = response.headers.authority.slice(1, -1);
 
       localStorage.setItem("lmsuserauthorities", userAuthority);
@@ -53,51 +54,69 @@ function Login() {
 
   return (
     <>
-      <div className="login-burger">
-        <div className="login-header">
-          <h1>Login</h1>
-        </div>
-        <form className="login-form" onSubmit={handleSubmit}>
-          <div className="mb-3">
-            <label htmlFor="exampleInputEmail1" className="form-label ">
-              Username
-            </label>
-            <input
-              id="username"
-              type="text"
-              className="form-control border border-secondary p-2 mb-2 border-opacity-75 input-box-shadow"
-              aria-describedby="emailHelp"
-              autoComplete="username"
-              value={username}
-              onChange={(e) => {
-                setUsername(e.target.value);
-              }}
-              required
-            />
-            <div id="emailHelp" className="form-text">
-              {/* We'll never share your email with anyone else. */}
+      <div className="login-root">
+        <section className="login-section-1">
+          <div className="login-section-1-container">
+            <div className="login-logo">{Logo(RedirectUrl.Homepage)}</div>
+            <div className="login-buttons"></div>
+          </div>
+        </section>
+        <section className="login-section-2">
+          <div className="login-container">
+            <div className="login-header">
+              <h1>Login</h1>
             </div>
+            <form className="login-form" onSubmit={handleSubmit}>
+              <div className="login-input-container"></div>
+              <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label ">
+                  Username
+                </label>
+                <input
+                  id="username"
+                  type="text"
+                  className="form-control border border-secondary p-2 mb-2 border-opacity-75 input-box-shadow"
+                  aria-describedby="emailHelp"
+                  autoComplete="username"
+                  value={username}
+                  onChange={(e) => {
+                    setUsername(e.target.value);
+                  }}
+                  required
+                />
+                <div id="emailHelp" className="form-text">
+                  {/* We'll never share your email with anyone else. */}
+                </div>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  type="password"
+                  className="form-control border border-secondary p-2 mb-2 border-opacity-75 input-box-shadow"
+                  autoComplete="current-password"
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                  required
+                />
+              </div>
+              {error ? (
+                <div className="error login-error">Login failed. Try again</div>
+              ) : null}
+              <div className="login-btn-container">
+                <button
+                  type="submit"
+                  className="btn btn-success login-btn-custom"
+                >
+                  Login
+                </button>
+              </div>
+            </form>
           </div>
-          <div className="mb-3">
-            <label htmlFor="exampleInputPassword1" className="form-label">
-              Password
-            </label>
-            <input
-              id="password"
-              type="password"
-              className="form-control border border-secondary p-2 mb-2 border-opacity-75 input-box-shadow"
-              autoComplete="current-password"
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-              required
-            />
-          </div>
-          {error ? <div className="error login-error">Login failed. Try again</div> : null}
-          <button type="submit" className="btn btn-success btn-login-custom">
-            Login
-          </button>
-        </form>
+        </section>
       </div>
     </>
   );
